042285a26bfa86a674c0b56e8364760de95d1e4f
ken <viethungpro97@gmail.com>
2024-03-04 10:11:03 +0700

    up

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,65 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+interface Env {
+  [key: string]: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env): Promise<Response> {
+    if (request.method == "POST") {
+			let data = await request.json()
+      const resp = await sendEmail(data, env);
+      const respText = await resp.text();
+			return new Response(respText, {
+				status: resp.status,
+				statusText: resp.statusText,
+				headers: { "content-type": "text/html" },
+			});
+    }
+		return new Response(null, {
+			status: 404
+		})
+  },
 };
+
+let sendEmail = async(data: any, env: Env) => {
+	let {sendTo, replyTo, replyToName = "", subject = "", message = ""} = data
+	let fromEmail = "ken@thangbom.life" // add your from email
+	let dkim_domain = "thangbom.life"
+	let dkim_selector = "mailchannels"
+	
+	let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+		method: "POST",
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify({
+			personalizations: [
+				{
+					to: [
+						{
+							email: sendTo,
+						},
+					],
+					dkim_domain,
+					dkim_selector,
+					dkim_private_key: env.DKIM_PRIVATE_KEY,
+				},
+			],
+			from: {
+				name: "Weaverse",
+				email: fromEmail,
+			},
+			reply_to: {
+				name: replyToName,
+				email: replyTo,
+			},
+			subject,
+			content: [
+				{
+					type: "text/plain",
+					value: message,
+				},
+			],
+		}),
+	});
+	return fetch(send_request);
+}
